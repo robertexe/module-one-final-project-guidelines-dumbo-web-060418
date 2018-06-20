7283b71ddff4821f52bff3efef3ce12e67db10f7
michermd <michermd@gmail.com>
2018-06-19 21:31:07 -0400

    droped old table and create a new migration for CreateMatches

--- db/migrate/20180620011116_create_matches.rb
+++ db/migrate/20180620011116_create_matches.rb
@@ -1,13 +1,13 @@
 class CreateMatches < ActiveRecord::Migration[5.0]
   def up
     create_table :matches do |t|
-      t.string :match_date
-      t.string :team_one
+      t.string :name_id
+      t.string :match_id
+      t.string :match_location
       t.integer :team_one_id
-      t.string :team_two
       t.integer :team_two_id
       t.integer :match_score
-      t.string :match_location
+      t.string :match_winner
     end
   end
 end

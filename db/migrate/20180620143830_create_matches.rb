0393a3f8090ae507faa836383026e28d5bbff715
michermd <michermd@gmail.com>
2018-06-20 10:41:06 -0400

    create new match table and seed the DB

--- db/migrate/20180620143830_create_matches.rb
+++ db/migrate/20180620143830_create_matches.rb
@@ -1,12 +1,13 @@
 class CreateMatches < ActiveRecord::Migration[5.0]
-  def up
+  def change
     create_table :matches do |t|
-      t.string :name_id
-      t.string :match_id
-      t.string :match_location
+      t.integer :match_name_id
+      t.string :match_name
       t.integer :team_one_id
+      t.string :team_one_name
       t.integer :team_two_id
-      t.integer :match_score
+      t.string :team_two_name
+      t.string :match_location
       t.string :match_winner
     end
   end

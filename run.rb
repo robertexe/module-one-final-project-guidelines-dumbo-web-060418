99f5ae13e1670354cf12fd7e9c9d1f5506112f19
michermd <michermd@gmail.com>
2018-06-21 06:41:57 -0400

    Solved if/elsif/esle logic for chosing team

--- run.rb
+++ run.rb
@@ -21,39 +21,45 @@ def choose_team
 	end
 	favorite = gets.chomp
 	# binding.pry
-	SoccerTeam.all.map do |team|
+	if SoccerTeam.all.map do |team|
 		team.country == favorite
-		puts "Great!! That's my favorite team too!!"
+	  end
+		puts "Great!! #{favorite} is my favorite team too!!"
 		sleep 1
-		puts "Now let's play the first match!!"
-		puts "Ready?"
-		puts "Yes / No"
+		puts "Now, let's play the first match!!"
+		puts "Are you ready?"
+		puts "|  Yes  |  No  |"
 		answer = gets.chomp
 		if answer == "Yes"
 			system "clear"
 			puts "Great! Now let's create a match!"
 			sleep 3
 			#Run the #create_match method
-			break
+
 		elsif answer == "No"
 			system "clear"
 			puts "Have a great day. Bye!"
 			sleep 3
-			break
+	
 		else
-			system "clear"
-			sleep 2
-			puts "Hey! What's the matter?"
-			puts "Is your finger shaking?"
-			sleep 2
-			puts "That is not a valid team!"
-			puts "|  🖥  Please type the name as it apears on your screen  🖥  |"
-			sleep 3
-			SoccerTeam.all.each do |el|
-				puts "  #{el.flag}  |  #{el.country}"
-			end
-			favorite = gets.chomp
+			puts "Well '#{answer}' is not avalid answer"
+			puts "Please enter Yes or No"
+			puts "Ready?"
+			puts "|  Yes  |  No  |"
+			answer = gets.chomp
 		end
+	else
+		system "clear"
+		puts "Hey! What's the matter?"
+		puts "Is your finger shaking?"
+		sleep 2
+		puts "#{favorite} is not a valid team!"
+		puts "|  🖥  Please type the name as it apears on your screen  🖥  |"
+		sleep 3
+		SoccerTeam.all.each do |el|
+			puts "  #{el.flag}  |  #{el.country}"
+		end
+		favorite = gets.chomp
 	end
 end
 choose_team
